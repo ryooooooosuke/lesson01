0a04533fa630355c25dbc75d28081815ef415f84
ryooooooosuke <ryosuke.git@gmail.com>
2023-02-08 00:02:04 +0900

    Fix file path and Timestamp

--- db/migration/20230207_create_post_table.php
+++ db/migration/20230207_create_post_table.php
@@ -1,13 +1,13 @@
 <?php
-    require('../../db_connect.php');
-    $db = dbConnect();
+    require('../../Database.php');
+    $db = Database::dbConnect();
 
     try {
         $sql = 'CREATE TABLE post (
             id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
             content VARCHAR(255) NOT NULL,
-            create_date TIMESTAMP,
-            update_date DATETIME
+            create_date DATETIME,
+            update_date TIMESTAMP
         )';
         $stmt = $db->prepare($sql);
         $stmt->execute();
